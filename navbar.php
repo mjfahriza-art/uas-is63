38c1cb99f3dcb8e90b20a1ae1010b1dc3186c17d
m_juhdi <mjfahriza@gmail.com>
2026-02-27 14:01:16 +0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-dark-subtle" data-bs-theme="dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="http://localhost/AUS-IS63">Navbar</a>
+    <a class="navbar-brand" href="http://localhost/UAS-IS63">Navbar</a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
         data-bs-target="#navbarSupportedContent">
